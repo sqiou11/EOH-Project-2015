ab3cec0a8de20ce393994d5c99baa1e6652949f1
Steven Qiou <sqiou11@gmail.com>
2015-03-06 14:33:58 -0600

    basic structure of website done

--- static/map.js
+++ static/map.js
@@ -3,7 +3,7 @@ var map;
 
 function initialize() {
   var mapOptions = {
-    zoom: 7,
+    zoom: 10,
     // Center the map on Chicago, USA.
     center: new google.maps.LatLng(41.879535, -87.624333)
   };
@@ -18,8 +18,46 @@ function initialize() {
   poly = new google.maps.Polyline(polyOptions);
   poly.setMap(map);
 
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+
   // Add a listener for the click event
   google.maps.event.addListener(map, 'click', addLatLng);
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
 
 /**
